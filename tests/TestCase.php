ec4d937332b92455341f373356b5a34888e82cb5
Yismen Jorge <yismen.jorge@gmail.com>
2025-06-23 12:50:12 -0400

    Add initial test case setup and expectations for Pest testing framework

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dainsys\FilamentHelpers\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    
+}
